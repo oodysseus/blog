39aaad2bccc8208406fea2c5d6f2b3133ae6db8c
oodysseus <oodysseus@163.com>
2019-03-03 18:43:29 +0800

    add jsdemo.js

---
+++ jsdemo.js
@@ -0,0 +1,24 @@
+var fs = require('fs')
+
+var dirName = process.argv[2]
+
+if (fs.existsSync(dirName)) {
+	console.log('Error : dir exists.')
+	
+	process.exit(1)
+}else{
+	fs.mkdirSync("./" + dirName)
+	process.chdir("./" + dirName)
+	
+	fs.mkdirSync('css')
+	fs.mkdirSync('js')
+
+	var str = "<!DOCTYPE>\n<title>Hello</title>\n<h1>Hi</h1>"
+	fs.writeFileSync("index.html",str)
+	str = "h1{color: red;}"
+	fs.writeFileSync("css/style.css",str)
+	str = 'var string = "Hello World"\nalert(string)'
+	fs.writeFileSync("js/main.js",str)
+	
+	process.exit(0)
+}
